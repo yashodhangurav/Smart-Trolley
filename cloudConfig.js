26e1899ea90b6d239d6daab5b8d68697230b7ba9
yashodhangurav <guravyashodhan@gmail.com>
2026-01-09 12:49:34 +0530

    initial commit

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,5 +1,6 @@
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
 
 
 cloudinary.config({
